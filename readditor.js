81ebdb8c72aeff0b6735578192f96293e7bbc7e4
CesarGBkR <yexotortuesprin@gmail.com>
2022-06-13 14:15:24 -0500

    Ading Variables to custom your input easier

--- readditor.js
+++ readditor.js
@@ -1,4 +1,7 @@
 var after = ''
+const subreddit = 'preguntaleareddit' //You can change this value for any subreddit
+const aks = 3 //Change this value to change the number of Ask in your output
+const nReplys = 3 //Change this value to change the number of replys for ask in your output
 
 function fetchAskreddit(){
     if (document.getElementById('Pregunta')){
@@ -7,31 +10,34 @@ function fetchAskreddit(){
 
     let parentdiv = document.createElement('div')
     parentdiv.id = 'Pregunta'
-    fetch(`https://www.reddit.com/r/AskReddit/.json?after=${after}`)
+    fetch(`https://www.reddit.com/r/${subreddit}/.json?after=${after}`)
     .then(response => response.json())
     .then(body =>{
         after = body.data.after
-        for(let index = 0; index<body.data.children.length; index++){
+        for(let index = 1; index<aks; index++){
             let url  = body.data.children[index].data.url
-            let authorReply = document.createElement('h4')
-            let reply = document.createElement('p')
+            let div = document.createElement('div')
+            let title = document.createElement('h2')
+            let author = document.createElement('h4')
+
             fetch(`${url}.json`)
             .then(response => response.json())
             .then(body =>{
-                authorReply.textContent = `${body[1].data.children[0].data.author} reply:`
-                reply.textContent = body[1].data.children[0].data.body
+                for(let i = 0; i<nReplys; i++){
+                    let xReply = document.createElement('h4')
+                    let yReply = document.createElement('p')
+                    xReply.textContent =  `${body[1].data.children[i].data.author} reply:`
+                    yReply.textContent = body[1].data.children[i].data.body
+                    div.appendChild(xReply)
+                    div.appendChild(yReply)
+                }
             })
-            let div = document.createElement('div')
-            let title = document.createElement('h3')
-            let author = document.createElement('h4')
-
+            
             title.textContent = body.data.children[index].data.title
             author.textContent = `${body.data.children[index].data.author} ask:`
             
             div.appendChild(author)
-            div.appendChild(title)
-            div.appendChild(authorReply)
-            div.appendChild(reply)   
+            div.appendChild(title)   
             parentdiv.appendChild(div)
         }
         document.body.appendChild(parentdiv)
